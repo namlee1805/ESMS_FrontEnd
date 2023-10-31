e078ae357874c5ea0753a934647c38d753d49aa3
Namlee's Macbook <namleesmacbook@MacBook-Pro-cua-Namlees.local>
2023-10-31 19:19:36 +0700

    upload profile student

--- src/App.js
+++ src/App.js
@@ -1,68 +1,18 @@
-// import { useEffect } from "react";
-// import {
-//   Routes,
-//   Route,
-//   useNavigationType,
-//   useLocation,
-// } from "react-router-dom";
-// import DesktopTry from "./pages/DesktopTry";
-
-// function App() {
-//   const action = useNavigationType();
-//   const location = useLocation();
-//   const pathname = location.pathname;
-
-//   useEffect(() => {
-//     if (action !== "POP") {
-//       window.scrollTo(0, 0);
-//     }
-//   }, [action, pathname]);
-
-//   useEffect(() => {
-//     let title = "";
-//     let metaDescription = "";
-
-//     switch (pathname) {
-//       case "/":
-//         title = "";
-//         metaDescription = "";
-//         break;
-//     }
-
-//     if (title) {
-//       document.title = title;
-//     }
-
-//     if (metaDescription) {
-//       const metaDescriptionTag = document.querySelector(
-//         'head > meta[name="description"]'
-//       );
-//       if (metaDescriptionTag) {
-//         metaDescriptionTag.content = metaDescription;
-//       }
-//     }
-//   }, [pathname]);
-
-//   return (
-//     <Routes>
-//       <Route path="/" element={<DesktopTry />} />
-//     </Routes>
-//   );
-// }
-// export default App;
-
 
 import React, { useEffect, useState } from "react";
 import { Routes, Route, useNavigationType, useLocation } from "react-router-dom";
-import Desktop_login from "./pages/Desktop_login";
-import Moobile_login from "./pages/Mobile_login";
+import Desktop_login from "./pages/LoginPage/Desktop_login";
+import Moobile_login from "./pages/LoginPage/Mobile_login";
+import ProStudent_Desktop from "./pages/ProStudent/ProStudent_Desktop";
+import ProStudent_tablet from "./pages/ProStudent/ProStudent_tablet";
+import ProStudent_mobile from "./pages/ProStudent/ProStudent_mobile";
 
 function App() {
   const action = useNavigationType();
   const location = useLocation();
   const pathname = location.pathname;
 
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 500);
 
   useEffect(() => {
     const handleResize = () => {
@@ -112,14 +62,23 @@ function App() {
     }
   }, [pathname]);
 
+  //Conditionally render the appropriate component based on screen width
+  let ProfileComponent;
+  if (window.innerWidth >= 1200) {
+    ProfileComponent = ProStudent_Desktop;
+  } else if (window.innerWidth >= 840) {
+    ProfileComponent = ProStudent_tablet;
+  } else {
+    ProfileComponent = ProStudent_mobile;
+  }
+
   return (
     <Routes>
-      <Route
-        path="/"
-        element={isMobile ? <Moobile_login /> : <Desktop_login />}
-      />
+      <Route path="/" element={isMobile ? <Moobile_login /> : <Desktop_login />} />
+      <Route path="/profile" element={<ProfileComponent />} />
+      {/* <Route path="/profile" element={isMobile ? <ProStudentMobile /> : (window.innerWidth >= 840 ? <ProStudentTablet /> : <ProStudentDesktop />)} /> */}
     </Routes>
   );
-}
 
+  }
 export default App;
---
+++ src/pages/ProStudent/ProStudent_Desktop.js
@@ -0,0 +1,151 @@
+import React, { useState, useEffect, useCallback } from "react"; 
+import "./ProStudent_Desktop.css";
+
+const ProStudent_Desktop = () => {
+  const [studentData, setStudentData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('http://localhost:8088/students')
+    .then(response => response.json())
+    .then(data => {
+      setStudentData(data);
+      setLoading(false);
+    })
+    .catch(error => {
+      setLoading(false);
+    });
+  }, []);
+
+  //All links
+
+  const onAvatarProClick = useCallback(() => {
+    window.location.href('http://localhost:3000/ProStudent');
+  }, []);
+
+  const onUserProClick = useCallback(() => {
+    window.location.href('http://localhost:3000/ProStudent');
+  }, []);
+
+  const onExamStuClick = useCallback(() => {
+    window.location.href('http://localhost:3000/ExStudent');
+  }, []);
+  
+  const onProStuClick = useCallback(() => {
+    window.location.href('http://localhost:3000/ProStudent');
+  }, []);
+
+  //use Google report => change link
+  const onReportStuClick = useCallback(() => {
+    window.open('http://localhost:3000/ProStudent');
+  }, []);
+
+  //logout => redirect login page
+  const onLogoutBtnClick = useCallback(() => {
+    window.location.href('http://localhost:3000/');
+  }, []);
+
+  return (
+    <div className="prostudent">
+      <div className="navbar">
+        <div className="fpt">
+          <div className="fpt-child" />
+          <div className="fpt-item" />
+          <div className="frame">
+            <div className="exam-schedule">Exam schedule management system</div>
+            <div className="fpt-university">FPT UNIVERSITY</div>
+          </div>
+        </div>
+        <button className="avatar" onClick={onAvatarProClick}>
+          <img className="avt-icon" alt="" src="/avt@2x.png" />
+          <div className="student-name">
+            <p className="student">Student</p>
+            <p className="student"> Name</p>
+          </div>
+        </button>
+      </div>
+      <div className="row">
+        <div className="menu">
+          <button className="user-profile" onClick={onUserProClick}>
+            <img
+              className="profile-image-icon"
+              alt=""
+              src="/profile-image@2x.png"
+            />
+            <div className="name-parent">
+              <b className="name1">Tin Dinh</b>
+              <div className="tittle">Real Estate Builders</div>
+            </div>
+          </button>
+          <div className="options">
+            <div className="exams-parent">
+              <button className="exams" onClick={onExamStuClick}>
+                <div className="exams-inner">
+                  <div className="icon-parent">
+                    <img className="icon" alt="" src="/icon.svg" />
+                    <div className="exams1">Exams</div>
+                  </div>
+                </div>
+              </button>
+              <button className="profile" onClick={onProStuClick}>
+                <div className="exams-inner">
+                  <div className="template-parent">
+                    <img className="template-icon" alt="" src="/template.svg" />
+                    <div className="profile1">Profile</div>
+                    <div className="frame-child" />
+                  </div>
+                </div>
+              </button>
+              <button className="reports" onClick={onReportStuClick}>
+                <div className="exams-inner">
+                  <div className="icon-group">
+                    <img className="icon1" alt="" src="/icon1.svg" />
+                    <div className="reports1">Reports</div>
+                  </div>
+                </div>
+              </button>
+            </div>
+          </div>
+          <button className="logout" onClick={onLogoutBtnClick}>
+            <div className="logout-wrapper">
+              <b className="logout1">
+                <span className="logout-txt">
+                  <p className="student">Logout</p>
+                </span>
+              </b>
+            </div>
+          </button>
+        </div>
+        <div className="body">
+          <b className="profile2">Profile</b>
+          <div className="student-id">Student ID:</div>
+          <div className="se173049-wrapper">
+            <div className="se173049">{loading ? 'Loading...' : studentData?.studentID}</div>
+          </div>
+          <div className="full-name">Full Name:</div>
+          <div className="se173049-wrapper">
+            <div className="on-nh-tn">{loading ? 'Loading...' : studentData?.studentName}</div>
+          </div>
+          <div className="student-id">Email:</div>
+          <div className="tinddse173049fpteduvn-wrapper">
+            <div className="tinddse173049fpteduvn">
+              {loading ? 'Loading...' : studentData?.studentEmail}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bot">
+        <div className="report">
+          <button className="report-child" onClick={onReportStuClick}/>
+          <div className="report1">Report</div>
+          <div className="image-1-wrapper">
+            <img className="image-1-icon" alt="" src="/image-1@2x.png" />
+          </div>
+        </div>
+        <img className="logofpt-icon" alt="" src="/logofpt@2x.png" />
+      </div>
+    </div>
+  );
+};
+
+export default ProStudent_Desktop;
